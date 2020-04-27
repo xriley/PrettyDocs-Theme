e81a3e143d1cb725635ffb46a07c31a7c826873f
Xiaoying Riley <xiaoying@3rdwavemedia.com>
2020-04-27 02:57:20 +0100

    Updated libs and license info

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ If you'd like to **use the template without the footer attribution**, you can [b
 
 
 ## Latest Version
-**v2.1.1** - 10 May 2019
+**v2.2** - 27 April 2020
 
 [Changelog](https://themes.3rdwavemedia.com/bootstrap-templates/startup/prettydocs-free-bootstrap-theme-for-developers-and-startups/?target=changelog)
 
--- assets/plugins/jquery-scrollTo/jquery.scrollTo.js
+++ assets/plugins/jquery-scrollTo/jquery.scrollTo.js
@@ -1,11 +1,11 @@
 /*!
  * jQuery.scrollTo
- * Copyright (c) 2007-2015 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
+ * Copyright (c) 2007 Ariel Flesler - aflesler ○ gmail • com | https://github.com/flesler
  * Licensed under MIT
- * http://flesler.blogspot.com/2007/10/jqueryscrollto.html
+ * https://github.com/flesler/jquery.scrollTo
  * @projectDescription Lightweight, cross-browser and highly customizable animated scrolling with jQuery
  * @author Ariel Flesler
- * @version 2.1.1
+ * @version 2.1.2
  */
 ;(function(factory) {
 	'use strict';
@@ -35,7 +35,7 @@
 	function isWin(elem) {
 		return !elem.nodeName ||
 			$.inArray(elem.nodeName.toLowerCase(), ['iframe','#document','html','body']) !== -1;
-	}		
+	}
 
 	$.fn.scrollTo = function(target, duration, settings) {
 		if (typeof duration === 'object') {
@@ -68,7 +68,7 @@
 			var win = isWin(this),
 				elem = win ? this.contentWindow || window : this,
 				$elem = $(elem),
-				targ = target, 
+				targ = target,
 				attr = {},
 				toff;
 
@@ -83,9 +83,9 @@
 					}
 					// Relative/Absolute selector
 					targ = win ? $(targ) : $(targ, elem);
-					if (!targ.length) return;
 					/* falls through */
 				case 'object':
+					if (targ.length === 0) return;
 					// DOMElement / jQuery
 					if (targ.is || targ.style) {
 						// Get the real position of the target
@@ -184,7 +184,7 @@
 	}
 
 	// Add special hooks so that window scroll properties can be animated
-	$.Tween.propHooks.scrollLeft = 
+	$.Tween.propHooks.scrollLeft =
 	$.Tween.propHooks.scrollTop = {
 		get: function(t) {
 			return $(t.elem)[t.prop]();
